e1ed9a3b35fcdd20bdd38386f8fc218bda8b289f
Paweł Żentała <zentala@gmail.com>
2024-06-22 13:59:57 +0200

    codelab starting point

---
+++ src/app/app.config.ts
@@ -0,0 +1,33 @@
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
+import { provideFirestore, getFirestore, connectFirestoreEmulator } from '@angular/fire/firestore';
+import { provideFunctions, getFunctions, connectFunctionsEmulator} from '@angular/fire/functions';
+import { provideMessaging, getMessaging } from '@angular/fire/messaging';
+import { provideStorage, getStorage, connectStorageEmulator } from '@angular/fire/storage';
+import { routes } from './app.routes';
+import { provideRouter } from '@angular/router';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { initializeAppCheck, ReCaptchaEnterpriseProvider, provideAppCheck } from '@angular/fire/app-check';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getRemoteConfig, provideRemoteConfig } from '@angular/fire/remote-config';
+import { getVertexAI, provideVertexAI } from '@angular/fire/vertexai-preview';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    importProvidersFrom(
+      provideFirebaseApp(() => initializeApp(environment.firebase)),
+      provideFirestore(() => getFirestore()),
+      provideAuth(() => getAuth()),
+      provideFunctions(() => getFunctions()),
+      provideStorage(() => getStorage()),
+      provideMessaging(() => getMessaging())
+    ),
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"scrum-my-life","appId":"1:469117218042:web:99fb353c7256ee1c14ad43","databaseURL":"https://scrum-my-life-default-rtdb.europe-west1.firebasedatabase.app","storageBucket":"scrum-my-life.appspot.com","apiKey":"AIzaSyAb_lFvvC32by5AIB4mDruj4GHjUgBHsGs","authDomain":"scrum-my-life.firebaseapp.com","messagingSenderId":"469117218042","measurementId":"G-GFTXMLE63T"})), provideAuth(() => getAuth()), provideAnalytics(() => getAnalytics()), ScreenTrackingService, UserTrackingService, provideAppCheck(() => {
+  // TODO get a reCAPTCHA Enterprise here https://console.cloud.google.com/security/recaptcha?project=_
+  const provider = new ReCaptchaEnterpriseProvider(/* reCAPTCHA Enterprise site key */);
+  return initializeAppCheck(undefined, { provider, isTokenAutoRefreshEnabled: true });
+}), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()), provideFunctions(() => getFunctions()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage()), provideRemoteConfig(() => getRemoteConfig()), provideVertexAI(() => getVertexAI())
+  ],
+};
